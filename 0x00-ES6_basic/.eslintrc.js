5e86904f4427dab947d1de7f6cd6aa59b2297809
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-06-26 18:00:54 +0000

    const

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,18 +1,35 @@
-{
-  "env": {
-    "browser": true,
-    "es2021": true,
-    "node": true,
-    "jest": true
+module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
   },
-  "extends": ["eslint:recommended", "plugin:jest/recommended"],
-  "parserOptions": {
-    "ecmaVersion": 12,
-    "sourceType": "module"
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-    "rules": {
-	"semi": "error",
-    "quotes": ["error", "single"],
-    "indent": ["error", 2]
-  }
-}
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
+};
--- 0x00-ES6_basic/0-constants.test.js
+++
@@ -1,6 +0,0 @@
-// 0-constants.test.js
-import { taskFirst } from './0-constants.js';
-
-test('taskFirst should return the expected string', () => {
-  expect(taskFirst()).toBe('I prefer const when I can.');
-});
