e70a32d5425ae24b896d55fe52c295cf047ea48c
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-06-28 09:26:53 +0000

    3 handlers

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,8 @@
 /* eslint-disable no-unused-vars */
-/* eslint-disable no-console */
+/*eslint-disable no-console*/
 /* eslint-disable no-var */
 export default function handleResponseFromAPI(promise) {
-  return promise
-    .then(() => ({ status: 200, body: 'Success' }))
-    .catch(() => Error())
-    .finally(() => { console.log('Got a response from the API'); });
+    return promise.then(() => ({status: 200, body: 'success'}))
+	.catch(() =>new Error())
+	.finally(() => { console.log('Got a response from the API'); });
 }
