d4abeee2bd90db31969d66ea92f43bec1b15b662
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-06-28 08:39:36 +0000

    added a handler then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,13 @@
 export default function handleResponseFromAPI(promise) {
   return new Promise((resolve, reject) => {
     if (promise) {
-      console.log('Got a response from the API');
       resolve({ status: 200, body: 'Success' });
     } else {
       reject(new Error());
     }
-  });
+  })
+    .then((response) => {
+      console.log('Got a response from the API');
+      return response;
+    });
 }
