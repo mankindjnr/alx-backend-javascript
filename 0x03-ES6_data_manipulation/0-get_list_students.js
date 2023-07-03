2169192074e2875cedcea7c65d9f061169d0c325
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-07-03 09:25:30 +0000

    basic list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,19 @@
+export default function getListStudents() {
+  const one = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Fransisco',
+  };
+  const two = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
+  const three = {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Fransisco',
+  };
+
+  return [one, two, three];
+}
