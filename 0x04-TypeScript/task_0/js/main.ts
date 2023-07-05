96f76cdf1aa0496ff87bffac0c8b69549f35dd87
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-07-05 13:43:48 +0400

    typescript

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,20 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+const student1: Student = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 24,
+    location: 'San Francisco'
+};
+
+const student2: Student = {
+    firstName: 'Jane',
+    lastName: 'Doe',
+    age: 24,
+    location: 'San Francisco'
+};
