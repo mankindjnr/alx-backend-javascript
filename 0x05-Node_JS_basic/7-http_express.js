cc0b978fe1b5900f67390c53383328a0e3c9c296
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-08-22 10:35:51 +0000

    complex express server

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const fileRead = require('./3.dup');
-const app = express()
+
+const app = express();
 const port = 1245;
 
-  app.get('/', (req, res) => {
-    res.end('Hello Holberton School!');
-  });
+app.get('/', (req, res) => {
+  res.end('Hello Holberton School!');
+});
 
-  app.get('/students', (req, res) =>{
-    fileRead('database.csv')
-      .then((result) => {
-        res.end(`This is the list of our students\n${result}`);
-      })
-      .catch((err) => {
-        console.error(err.message);
-      });
-  })
+app.get('/students', (req, res) => {
+  fileRead('database.csv')
+    .then((result) => {
+      res.end(`This is the list of our students\n${result}`);
+    })
+    .catch((err) => {
+      console.error(err.message);
+    });
+});
 
 app.listen(port, () => {
   console.log('...');
