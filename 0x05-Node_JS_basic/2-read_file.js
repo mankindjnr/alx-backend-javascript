f1869a307c988a962208bdcb6a3f1a22bce2a35b
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-08-22 07:23:59 +0000

    erading a file synchronously with node

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
-    const lines = data.split('\n').filter(line => line.trim() !== ''); // Remove empty lines
+    const lines = data.split('\n').filter((line) => line.trim() !== ''); // Remove empty lines
 
     if (lines.length === 0) {
       throw new Error('Database is empty');
@@ -21,32 +21,29 @@ function countStudents(path) {
 
     const fields = [];
 
-    for (const line of lines.slice(1)){
-        const values = line.split(',');
-        const major = values[3].trim();
-        if (!fields.includes(major))
-        fields.push(major);
-
+    for (const line of lines.slice(1)) {
+      const values = line.split(',');
+      const major = values[3].trim();
+      if (!fields.includes(major)) fields.push(major);
     }
 
-    for (const field of fields){
-      const current_field = [];
-      for (const line of lines.slice(1)){
+    for (const field of fields) {
+      const currentField = [];
+      for (const line of lines.slice(1)) {
         const values = line.split(',');
         const major = values[3].trim();
 
-        if (major === field){
-          current_field.push(values[0].trim());
+        if (major === field) {
+          currentField.push(values[0].trim());
         }
       }
 
-      console.log(`Number of students in ${field}: ${current_field.length}. List: ${current_field.join(", ")}`);
+      console.log(`Number of students in ${field}: ${currentField.length}. List: ${currentField.join(', ')}`);
     }
-
   } catch (error) {
-    throw new Error(`Cannot load the database`);
+    throw new Error('Cannot load the database');
   }
 }
 
 // Call the function with the path to the database file
-module.exports= countStudents;
+module.exports = countStudents;
