7017c2e57a35358fecc69b75e39609de83d60378
mankindjnr <kamos8646@stu.kemu.ac.ke>
2023-08-22 08:56:15 +0000

    complex server

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,21 +1,21 @@
-const http = require("http");
-const fileRead = require('./3-read_file_async')
+const http = require('http');
+const fileRead = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-    if (req.url === '/') {
-        res.end("Hello Holberton School!");
-    } else if (req.url === "/students") {
-        res.end("This is the list of our students");
-        res.end(fileRead("database.csv"));
-    }
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.end('This is the list of our students');
+    fileRead('database.csv');
+  }
 });
 
 const port = 1245;
 
 app.listen(port, () => {
-    console.log("...");
+  console.log('...');
 });
 
 module.exports = app;
